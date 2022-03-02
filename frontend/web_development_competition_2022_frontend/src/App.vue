b554cfb9c1447e5b0d225be9988c51a0fc7a418d
[Students] Renars Gausins <03DPRGausins@rvt.lv>
2022-03-02 09:51:57 +0200

    .

---
+++ frontend/web_development_competition_2022_frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+
+<style>
+#app {
+  max-width: 1500px;
+  width: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+
+</style>
